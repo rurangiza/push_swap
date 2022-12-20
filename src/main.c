3c73d70f3997f77d64181278308287cd2bc75ce1
Arsene Rurangiza <arurangi@shi-r10-p3.s19.be>
2022-12-20 15:16:50 +0100

    added arg type indicator

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 09:26:12 by Arsene            #+#    #+#             */
-/*   Updated: 2022/12/20 15:00:26 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/12/20 15:15:32 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,29 @@
 int main(int arg_count, char **argv)
 {
     t_list  *stack;
-    t_list  *head = NULL;
     int     index;
     char    **arg_list;
+    int     arg_type;
 
     if (arg_count == 1)
         return (error_msg(1, "No numbers to sort")); // Remove error message
     
     index = 0;
     if (arg_count == 2)
-        arg_list = ft_split(argv, ' ');
+    {
+        arg_list = ft_split(argv, ' '); // Free later
+        arg_type = 0;
+    }
     else
     {
         index = 1;
         arg_list = argv;
+        arg_type = 1;
     }
 
     while (arg_list[index])
     {
-        if (valid_number(arg_list, index))
+        if (valid_number(arg_list, index, arg_type))
         {
             push(ft_atoi(arg_list[index]), stack);
             index++;
--- src/utils/input_check.c
+++ src/utils/input_check.c
@@ -6,14 +6,14 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 13:49:33 by arurangi          #+#    #+#             */
-/*   Updated: 2022/12/20 13:53:48 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/12/20 15:16:15 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-// Valid number
-int valid_number(char **arg_list, int index)
+/* Valid number */
+int valid_number(char **arg_list, int index, int arg_type)
 {
     int     number;
     char    *strnum;
@@ -23,18 +23,19 @@ int valid_number(char **arg_list, int index)
     strnum = arg_list[index];
     while (strnum[i] != '\0')
     {
-        if (!ft_isdigit(strnum[i]) && strnum[i] != '-' && strnum[i] != '+')
+        if (!ft_isdigit(strnum[i]) && !ft_issign(strnum[i]))
             return (1, "argument nº%d is not a valid digit", index);
         i++;
     }
     number = ft_atoi(strnum);
     if (number < INT_MIN || number > INT_MAX)
         return (0, "number bigger/lower than INT_MAX/INT_MIN");
-    if (!first_encounter(number, arg_list, index)) // Replace with comparaison with previous numbers in the stack for quick traversal
+    if (!first_encounter(number, arg_list, index, arg_type)) // Replace with comparaison with previous numbers in the stack for quick traversal
         return (0, "there are duplicates of %d", number);
+    return (1);
 }
 
-// Add an element to the top of a stack
+/* Add an element to the top of a stack */
 void    push(int node_content, t_list **stack)
 {
     t_list *node;
@@ -43,16 +44,18 @@ void    push(int node_content, t_list **stack)
     if (!node)
     {
         error_msg(0, "couldn't create new node");
+        // Shoulw I free?
         return ;
     }
     ft_lstadd_front(stack, node);
 }
 
-int	first_encounter(int number, char *arguments, int position)
+/* BEWARE!!! index will start with 1 or 0 depending on arg_list type */
+int	first_encounter(int number, char *arguments, int position, int arg_type)
 {
 	int index;
 
-	index = 1;
+	index = arg_type;
 	while (index < position)
 	{
 		if (ft_atoi(arguments[index]) == number)
@@ -60,36 +63,4 @@ int	first_encounter(int number, char *arguments, int position)
 		index++;
 	}
 	return (1);
-}
-
-int    extract_numbers(int arg_count, char **arg_list, t_list *stack)
-{
-    int     index;
-    char    *strnums;
-    int     sign;
-    int     number;
-
-    /*  Two options:
-        - progname + number => nothing to sort
-        - progname + string of numbers => extract numbers
-            ./push_swap "12 54 844 -62"
-    */
-    strnums = arg_list[1];
-    index = 0;
-    while (strnums[index] != '\0')
-    {
-        if (!ft_isdigit(strnums[index]) && !ft_isspace(strnums[index]) && !issign(strnums[index]))
-            return (error_msg(0, "contains invalid characters"));
-        
-        number = ft_atoi(strnums + index);
-        push(number, stack);
-        // sign = 1;
-        
-        while (ft_isspace(strnums[index]))
-            index++;
-        while (ft_issign(strnums[index]))
-            index++;
-        while (ft_isdigit(strnums[index]))
-            index++;
-    }
 }
