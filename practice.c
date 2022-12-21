23ecf19316980d8ba81f87ed665d3b1197e56bee
Arsène Rurangiza <a.rurangiza@gmail.com>
2022-12-21 21:14:26 +0100

    added random number generator

--- practice.c
+++ practice.c
@@ -3,15 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   practice.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Arsene <Arsene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 15:36:31 by arurangi          #+#    #+#             */
-/*   Updated: 2022/12/21 17:05:19 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/12/21 20:42:02 by Arsene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+3 numbers -> 6 operations
+5 numbers -> 12 operations
+100 numbers -> 
+*/
+
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 
 /* UTILS */
 void	summury(int *unsorted, int size, int operations)
@@ -20,6 +29,46 @@ void	summury(int *unsorted, int size, int operations)
 	for (int i = 0; i < size; i++)
 		printf("%d ", unsorted[i]);
 	printf(" \033[33m(%d operations)\033[0m\n", operations);
+}
+
+int	first_encounter(int number, int *list, int position)
+{
+	int i;
+	
+	i = 0;
+	while (i < position)
+	{
+		if (number == list[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	*random_numbers(int size, int threshold)
+{
+	int	index;
+	int	*list;
+	int	sign;
+	int	number;
+
+	list = malloc(sizeof(int) * size);
+	srand(time(0));
+	index = 0;	
+	while (index < size)
+	{
+		sign = 1;
+		if (rand() % 3 == 1)
+			sign = -1;
+		number = (rand() % threshold) * sign;
+		while (!first_encounter(number, list, index))
+		{
+			number = (rand() % threshold) * sign;
+		}
+		list[index] = number;
+		index ++;
+	}
+	return (list);
 }
 
 /* OPERATIONS */
@@ -57,29 +106,28 @@ void	bubble_sort(int	*unsorted, int size)
 		}
 	}
 	// Print results
-	summury(unsorted, size, operations);
+	printf("\n=> %d operations\n", operations);
+	//summury(unsorted, size, operations);
 }
-
-/*
-3 numbers -> 6 operations
-5 numbers -> 12 operations
-100 numbers -> 
-*/
 
 /* MAIN */
 int main(void)
 {
-	//int	unsorted_10[] = {20, 98, 1, 14, 2, 38, 7, 5, 3, 10};
-	int	unsorted_3[] = {2, 3, 1}; // 2 3 1 -> 2 1 3 -> 1 2 3
+	int	size = 50;
+	int	*unsorted = random_numbers(size, 100);
+
+	printf("\033[31mBefore:\033[0m ");
+	for (int i = 0; i < size; i++)
+		printf("%d ", unsorted[i]);
+	write(1, "\n", 1);
 	
-	bubble_sort(unsorted_3, 3);
+	bubble_sort(unsorted, size);
 	
+	printf("\n\033[32mAfter:\033[0m ");
+	for (int i = 0; i < size; i++)
+		printf("%d ", unsorted[i]);
+	write(1, "\n", 1);
+
+	free(unsorted);
 	return (0);
 }
-
-/*
-
-ra : rotate a
-
-
-*/
