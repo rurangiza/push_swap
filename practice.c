375fd17ccd086bb90688c91ba7752c47cbff9004
Arsene Rurangiza <arurangi@shi-r11-p5.s19.be>
2022-12-21 17:10:26 +0100

    create practice file

--- practice.c
+++ practice.c
@@ -6,12 +6,21 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 15:36:31 by arurangi          #+#    #+#             */
-/*   Updated: 2022/12/21 16:04:46 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/12/21 17:05:19 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+
+/* UTILS */
+void	summury(int *unsorted, int size, int operations)
+{
+	printf("Sorted: ");
+	for (int i = 0; i < size; i++)
+		printf("%d ", unsorted[i]);
+	printf(" \033[33m(%d operations)\033[0m\n", operations);
+}
 
 /* OPERATIONS */
 void	swap(int *a, int *b)
@@ -48,18 +57,29 @@ void	bubble_sort(int	*unsorted, int size)
 		}
 	}
 	// Print results
-	printf("Sorted: ");
-	for (int i = 0; i < size; i++)
-		printf("%d ", unsorted[i]);
-	printf(" \033[33m(%d operations)\033[0m\n", operations);
+	summury(unsorted, size, operations);
 }
+
+/*
+3 numbers -> 6 operations
+5 numbers -> 12 operations
+100 numbers -> 
+*/
 
 /* MAIN */
 int main(void)
 {
-	int	unsorted[] = {20, 98, 1, 14, 2, 38, 7};
-
-	bubble_sort(unsorted, 7);
+	//int	unsorted_10[] = {20, 98, 1, 14, 2, 38, 7, 5, 3, 10};
+	int	unsorted_3[] = {2, 3, 1}; // 2 3 1 -> 2 1 3 -> 1 2 3
+	
+	bubble_sort(unsorted_3, 3);
 	
 	return (0);
 }
+
+/*
+
+ra : rotate a
+
+
+*/
