0a23e6c60a2aa0b819df6f8160f0de69712fa1ee
Arsene Rurangiza <arurangi@shi-r10-p5.s19.be>
2023-01-05 12:14:21 +0100

    updated

--- src/sorting.c
+++ src/sorting.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 14:45:12 by arurangi          #+#    #+#             */
-/*   Updated: 2023/01/05 10:42:13 by arurangi         ###   ########.fr       */
+/*   Updated: 2023/01/05 12:07:45 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	sort_stack(t_node *stack_a, t_node *stack_b)
 		handle_3(stack_a, stack_b);
 	else if (stack_size <= 5)
 		handle_5(stack_a, stack_b);
-	else if (stack_size <= 100)
+	else if (stack_size <= 500)
 		handle_100(stack_a, stack_b);
 	else
 		quicksort(stack_a, stack_b, 0, stack_size);
